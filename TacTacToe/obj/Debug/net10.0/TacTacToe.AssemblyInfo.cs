9cfbac85036c87cbf31c32d592e82247b079d034
shudonz <shudonz@gmail.com>
2026-04-18 14:04:18 -0500

    Update .gitignore and move project to TacTacToeHub
    Replaced .gitignore with standard rules for VS, .NET, Node.js, and secrets. Updated project paths from TacTacToe to TacTacToeHub. Refreshed assembly version and cache files to reflect latest build and source changes.

--- TacTacToe/obj/Debug/net10.0/TacTacToe.AssemblyInfo.cs
+++ TacTacToe/obj/Debug/net10.0/TacTacToe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TacTacToe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+37b812c42555fae7db343eed39d3e6ef7ea7095a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8124056287f343b44c6adadef428d71d99bd7c61")]
 [assembly: System.Reflection.AssemblyProductAttribute("TacTacToe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TacTacToe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
